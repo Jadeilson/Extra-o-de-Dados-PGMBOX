2108313879ffd46fbdf944e009ddb15666f85430
jadeilson <jadeilson.correia@yahoo.com.br>
2017-06-08 01:48:54 -0300

    Teste alteração box

--- src/br/com/extracaoDadosPgmbox/CadastroTeste.java
+++ src/br/com/extracaoDadosPgmbox/CadastroTeste.java
@@ -173,7 +173,7 @@ public class CadastroTeste {
 		textOrientacao.setColumns(10);
 		
 		JComboBox comboBoxBase = new JComboBox();
-		comboBoxBase.setModel(new DefaultComboBoxModel(new String[] {"PGMP0001", "PGMP0002", "PGMP0003", "PGMP0004", "PGMP0005"}));
+		comboBoxBase.setModel(new DefaultComboBoxModel(new String[] {"PGMP0001", "PGMP0002", "PGMP0003", "PGMP0004"}));
 		
 		
 		comboBoxBase.setBounds(6, 105, 100, 20);
